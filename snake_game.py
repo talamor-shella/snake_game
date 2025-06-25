a1d4707c5f4459ee45676d886f2236095d443422
talamor-shella <shellatalamor@gmail.com>
2025-06-25 21:01:30 +0800

    added constant variables

--- snake_game.py
+++ snake_game.py
@@ -4,6 +4,15 @@ from tkinter import *
 import random
 
 #constant variables for the game 
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPACE_SIZE = 50
+SPEED = 120
+BODY_PARTS = 3
+SNAKE_COLOR = "#00F000"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
 #abstract class game object
 #snake class inheriting from game object
 #food class inheriting from game object
